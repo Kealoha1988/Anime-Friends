3088ffba03e032a0c176bc8fda7f19569ece9d9c
Kealoha1988 <kealohaabe1988@gmail.com>
2021-01-06 14:37:06 -0500

    nav start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,11 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+
   get "/" do
     erb :welcome
   end
+
+
 
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,8 @@
+class UserController < ApplicationController
+  
+
+  get "/" do
+    erb :welcome
+  end
+  
+end
